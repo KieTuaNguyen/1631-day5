232e574c533ed9945eba84a67379ebc2c86a8487
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2022-10-08 09:32:44 +0700

    Added my project

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/product")
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_product_index", methods={"GET"})
+     */
+    public function index(Request $request, ProductRepository $productRepository): Response
+    {
+        $minPrice = $request->query->get('minPrice');
+        $maxPrice = $request->query->get('maxPrice');
+        // $product = $productRepository->findAll();
+        if ($minPrice && $maxPrice) {
+            $product = $productRepository->findAllPriceInRange($minPrice, $maxPrice);
+        } else {
+            $product = $productRepository->findAll();
+        }
+        // $product = $productRepository->findAllPriceInRange($minPrice, $maxPrice);
+        return $this->render('product/index.html.twig', [
+            'products' => $product,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="app_product_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, ProductRepository $productRepository): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productRepository->add($product, true);
+
+            return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('product/new.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    //create a function which show all product in price range
+
+    /**
+     * @Route("/phepcong", name="app_product_plus", methods={"GET", "POST"})
+     */
+    public function plus(Request $request): Response
+    {
+        $firstNumber = $request->get('a');
+        $secondNumber = $request->get('b');
+        $name = $request->get('name');
+
+        return new Response('<html><body>Tong: ' . ($firstNumber + $secondNumber) . 'Welcome  ' . $name . '</body></html>');
+    }
+
+    /**
+     * @Route("/{id}", name="app_product_show", methods={"GET"})
+     */
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_product_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Product $product, ProductRepository $productRepository): Response
+    {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productRepository->add($product, true);
+
+            return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_product_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Product $product, ProductRepository $productRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->request->get('_token'))) {
+            $productRepository->remove($product, true);
+        }
+
+        return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
